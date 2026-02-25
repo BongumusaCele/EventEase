1c549e54906688cc65d608eaf7129b25ae2d61b8
prospa <prospa.bongumusa@gmail.com>
2026-02-25 16:08:27 +0200

    Events Controller Crud operations

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -2,6 +2,7 @@
 using EventEase.Models;
 using EventEase.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace EventEase.Controllers
 {
@@ -21,40 +22,64 @@ namespace EventEase.Controllers
             return View(items);
         }
 
-        public IActionResult Create() {
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) { return NotFound(); }
 
+            var myEvents = await _contextEventEase.Events.Include(e => e.Venue).FirstOrDefaultAsync(e => e.EventId == id);
+
+            if (myEvents == null) { return NotFound(); }
+
+            return View(myEvents);
+        }
+
+        public async Task<IActionResult> CreateAsync() 
+        {
+            ViewBag.Venues = new SelectList(await _contextEventEase.Venues.ToListAsync(), "VenueId", "Name");
             return View();
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("EventId, Name, StartDateTime, EndDateTime, Description, VenueId")] Event item)
         {
             if (ModelState.IsValid)
             {
                 _contextEventEase.Add(item);
                 await _contextEventEase.SaveChangesAsync();
-                return RedirectToAction("Index");
+                return RedirectToAction(nameof(Index));
             }
             return View(item);
         }
 
-        public async Task<IActionResult> Edit(int id)
+        public async Task<IActionResult> Edit(int? id)
         {
-            var item = await _contextEventEase.Events.FirstOrDefaultAsync(x => x.EventId == id);
+            if (id == null) { return NotFound(); }
 
-            return View(item);
+            var ev = await _contextEventEase.Events.FindAsync(id);
+
+            if (ev == null) { return NotFound(); }
+
+            ViewBag.Venues = new SelectList(await _contextEventEase.Venues.ToListAsync(), "VenueId", "Name", ev.VenueId);
+
+            return View(ev);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Edit(int EventId, [Bind("EventId, Name, StartDateTime, EndDateTime, Description, VenueId")] Event item)
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Event item)
         {
-            if (ModelState.IsValid)
+            if (id != item.EventId) { return NotFound(); }
+
+            if (!ModelState.IsValid)
             {
-                _contextEventEase.Update(item);
-                await _contextEventEase.SaveChangesAsync();
-                return RedirectToAction("Index");
+                ViewBag.Venues = new SelectList(await _contextEventEase.Venues.ToListAsync(), "VenueId", "Name", item.VenueId);
+                return View(item);
             }
-            return View(item);
+
+            _contextEventEase.Update(item);
+            await _contextEventEase.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
 
         public async Task<IActionResult> Delete(int id)
@@ -64,6 +89,7 @@ namespace EventEase.Controllers
         }
 
         [HttpPost , ActionName("Delete")]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var item = await _contextEventEase.Events.FindAsync(id);
@@ -73,7 +99,7 @@ namespace EventEase.Controllers
                 _contextEventEase.Events.Remove(item);
                 await _contextEventEase.SaveChangesAsync();
             }
-            return RedirectToAction("Index");
+            return RedirectToAction(nameof(Index));
         }
     }
 }
